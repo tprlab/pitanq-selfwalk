a65cbcaa22675593587181a7521de58626476019
tprlab <dev@tprlab.com>
2019-05-20 15:06:53 -0400

    Improved to handle both file and folder

--- check_nn.py
+++ check_nn.py
@@ -1,11 +1,18 @@
 import sys
-from PIL import Image
 import numpy as np
 import cv2 as cv
+import os
 
 from keras.models import load_model
 from keras.models import model_from_json
 from keras.preprocessing.image import img_to_array 
+
+
+def handle_file(path):
+    img = cv.imread(path, cv.IMREAD_GRAYSCALE)
+    img = np.reshape(img,[1,64,64,dim])
+    return loaded_model.predict_classes(img)
+
 
 
 json_file = open('model.json', 'r')
@@ -19,21 +26,30 @@ loaded_model.load_weights("model.h5")
 print("Loaded model from disk")
 
 dim = 1
-target = int(sys.argv[2])
+target = None
+if len(sys.argv) > 2:
+    target = int(sys.argv[2])
 
 fails = 0
 all = 0
 
 root = sys.argv[1]
-for p in os.listdir(root):
-    img = cv2.imread(root + p, cv2.IMREAD_GRAYSCALE)
-    img = np.reshape(img,[1,64,64,dim])
-    c = loaded_model.predict_classes(img)
-    print p, c[0]
-    all += 1
-    if target is not None:
-        if c != target:
-            fails += 1
 
-print ("Failures", fails, "of", all)
+
+
+if os.path.isdir(root):
+    for p in os.listdir(root):
+        c = handle_file(root + p)
+        print (p, c[0])
+        all += 1
+        if target is not None:
+            if c != target:
+                fails += 1
+    print ("Failures", fails, "of", all)
+else:
+    c = handle_file(root)
+    print ("Prediction", c)
+
+
+
 
